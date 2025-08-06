de37d8d8424619ee3624d37e0a52a06be1e0c14b
abui0415 <anhbui0415@gmail.com>
2025-08-05 20:41:32 -0400

    Created Swift project

---
+++ theBasics/theBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  theBasics
+//
+//  Created by Anh-Thu Bui on 8/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
